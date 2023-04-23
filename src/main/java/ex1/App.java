3c11da0c26e1b4ee52dbf1c813560faee6e3553d
oleg <legik7127@gmail.com>
2023-04-23 21:23:40 +0600

    added some classes

--- src/main/java/ex1/App.java
+++ src/main/java/ex1/App.java
@@ -5,27 +5,29 @@ package ex1;
  */
 public class App {
     public static void main(String[] args) {
+        ButtonAction foo = new ButtonAction();
+        Btn button = new Btn(foo);
 
+        View v1 = new View(button);
 
     }
-
 }
 
-class Foo {
+class ButtonAction {
     public void method() {
-        System.out.println("Foo.method");
+        System.out.println("ButtonAction.method");
     }
 
 }
 
-class Doo extends Foo {
+class NewAction extends ButtonAction {
     @Override
     public void method() {
-        System.out.println("Doo.method");
+        System.out.println("NewAction.method");
     }
 }
 
-class Hoo extends Foo {
+class Hoo extends ButtonAction {
     @Override
     public void method() {
         System.out.println("Hoo.method");
@@ -33,18 +35,25 @@ class Hoo extends Foo {
 }
 
 class View {
-    Btn btn = new Btn();
+    private Btn btn;
+    public void click(){
+        btn.click();
+    }
+
+    public View(Btn current) {
+        this.btn = current;
+    }
 }
 
 class Btn {
-    Foo f;
+    ButtonAction action;
 
-    public Btn() {
-        f = new Foo();
+    public Btn(ButtonAction action) {
+        this.action = action;
     }
 
     public void click() {
-        f.method();
+        action.method();
     }
 
 }
