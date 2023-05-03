2e2a271358aef341e3b3653413c6f38f7b34fd77
oleg <legik7127@gmail.com>
2023-05-03 23:35:01 +0600

    added interface Boat

--- src/main/java/ex5/AppBoatStation.java
+++ src/main/java/ex5/AppBoatStation.java
@@ -3,24 +3,47 @@ package ex5;
 public class AppBoatStation {
     public static void main(String[] args) {
 
+        QuadrupleBoat q1 = new QuadrupleBoat();
+        q1.getInfo();
+
     }
 }
-interface Boat{
+
+interface Boat {
     int q = 0;
+    String boatMode = "";
 
-}
-class DoubleBoat implements Boat{
+    default void getInfo() {
+        System.out.println("Базовая модель лодки 1 местная");
 
-}
-class QuadrupleBoat implements Boat{
-
-}
-class CashDesk{
-    public void getPay(int numB, int hour){
-        System.out.println("Лодка номер "+ numB +" количество часов" + hour);
     }
-
 }
-class Payment{
+
+class DoubleBoat implements Boat {
+    public int price;
+    public String color;
+
+    public DoubleBoat(int price, String color) {
+        super();
+        this.price = price;
+        this.color = color;
+    }
+}
+
+class QuadrupleBoat implements Boat {
+    @Override
+    public void getInfo() {
+        System.out.println("У нас появилась 4-х местная лодка");
+    }
+}
+
+class CashDesk {
+    public void getPay(int numB, int hour) {
+        System.out.println("Лодка номер " + numB + "оплачена на количество часов " + hour);
+
+    }
+}
+
+class Payment {
 
 }
